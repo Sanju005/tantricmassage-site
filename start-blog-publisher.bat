9ade4fc62f1adb83d767cc36277de47596933ca5
Sanju <mrsanju005@gmail.com>
2026-05-20 21:05:57 +0800

    wer

--- start-blog-publisher.bat
+++ start-blog-publisher.bat
@@ -1,7 +1,9 @@
 @echo off
 cd /d "%~dp0"
-start "Blog Publisher Server" "%SystemRoot%\System32\cmd.exe" /k "cd /d \"%~dp0\" && node \"%~dp0scripts\blog-publisher-server.js\""
-timeout /t 3 /nobreak >nul
-start "" "http://127.0.0.1:8790/"
-
+echo Starting Blog Publisher...
+start "" "%SystemRoot%\System32\cmd.exe" /c "timeout /t 3 /nobreak >nul && start \"\" \"http://127.0.0.1:8790/\""
+"C:\Program Files\nodejs\node.exe" "%~dp0scripts\blog-publisher-server.js"
+echo.
+echo If you see this line, the server stopped.
+pause
 
