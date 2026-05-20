17ca6f7e9f819823880e87ea734ec0bb322deb68
Sanju <mrsanju005@gmail.com>
2026-05-20 19:56:17 +0800

    fil

--- start-blog-publisher.bat
+++ start-blog-publisher.bat
@@ -1,4 +1,5 @@
 @echo off
 cd /d "%~dp0"
+start "Blog Publisher Server" cmd /k "cd /d %~dp0 && node scripts\blog-publisher-server.js"
+timeout /t 2 /nobreak >nul
 start "" http://127.0.0.1:8787
-node scripts\blog-publisher-server.js
